dad2720e23417e70bcf5a7980fc523356e395b8d
ref2401 <refactor24@gmail.com>
2016-09-05 11:04:10 +0300

    quat: binary ops & unittests

--- src/cg/math/quat.h
+++ src/cg/math/quat.h
@@ -87,10 +87,15 @@ inline quat& quat::operator*=(const Numeric& val)
 
 inline quat& quat::operator*=(const quat& q)
 {
-	x = (a * q.x) + (x * q.a) + (y * q.z) - (z * q.y);
-	y = (a * q.y) + (y * q.a) + (z * q.x) - (x * q.z);
-	z = (a * q.z) + (z * q.a) + (x * q.y) - (y * q.x);
-	a = (a * q.a) - (x * q.x) - (y * q.y) - (z * q.z);
+	float xp = (a * q.x) + (x * q.a) + (y * q.z) - (z * q.y);
+	float yp = (a * q.y) + (y * q.a) + (z * q.x) - (x * q.z);
+	float zp = (a * q.z) + (z * q.a) + (x * q.y) - (y * q.x);
+	float ap = (a * q.a) - (x * q.x) - (y * q.y) - (z * q.z);
+
+	x = xp;
+	y = yp;
+	z = zp;
+	a = ap;
 	return *this;
 }
 
@@ -120,25 +125,62 @@ inline bool operator!=(const quat& lhs, const quat& rhs)
 	return !(lhs == rhs);
 }
 
-//template<typename Numeric>
-//inline quat operator*(quat q, const Numeric& val)
-//{
-//	static_assert(std::is_integral<Numeric>::value || std::is_floating_point<Numeric>::value,
-//		"Numeric type must be an integer or floating point value.");
-//	q *= val;
-//	return q;
-//}
-//
-//template<typename Numeric>
-//inline quat operator*(const Numeric& val, quat q)
-//{
-//	static_assert(std::is_integral<Numeric>::value || std::is_floating_point<Numeric>::value,
-//		"Numeric type must be an integer or floating point value.");
-//	q *= val;
-//	return q;
-//}
-//
-//inline quat operator*(quat lhs, const 
+inline quat operator+(const quat& lhs, const quat& rhs)
+{
+	return quat(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z, lhs.a + rhs.a);
+}
+
+inline quat operator-(const quat& lhs, const quat& rhs)
+{
+	return quat(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z, lhs.a - rhs.a);
+}
+
+inline quat operator-(const quat& q)
+{
+	return quat(-q.x, -q.y, -q.z, -q.a);
+}
+
+template<typename Numeric>
+inline quat operator*(const quat& q, const Numeric& val)
+{
+	static_assert(std::is_integral<Numeric>::value || std::is_floating_point<Numeric>::value,
+		"Numeric type must be an integer or floating point value.");
+	return quat(q.x * val, q.y * val, q.z * val, q.a * val);
+}
+
+template<typename Numeric>
+inline quat operator*(const Numeric& val, const quat& q)
+{
+	static_assert(std::is_integral<Numeric>::value || std::is_floating_point<Numeric>::value,
+		"Numeric type must be an integer or floating point value.");
+	return quat(q.x * val, q.y * val, q.z * val, q.a * val);
+}
+
+inline quat operator*(const quat& lhs, const quat& rhs)
+{
+	return quat(
+		(lhs.a * rhs.x) + (lhs.x * rhs.a) + (lhs.y * rhs.z) - (lhs.z * rhs.y),
+		(lhs.a * rhs.y) + (lhs.y * rhs.a) + (lhs.z * rhs.x) - (lhs.x * rhs.z),
+		(lhs.a * rhs.z) + (lhs.z * rhs.a) + (lhs.x * rhs.y) - (lhs.y * rhs.x),
+		(lhs.a * rhs.a) - (lhs.x * rhs.x) - (lhs.y * rhs.y) - (lhs.z * rhs.z)
+	);
+}
+
+template<typename Numeric>
+inline quat operator/(const quat& q, const Numeric& val)
+{
+	static_assert(std::is_integral<Numeric>::value || std::is_floating_point<Numeric>::value,
+		"Numeric type must be an integer or floating point value.");
+	return quat(q.x / val, q.y / val, q.z / val, q.a / val);
+}
+
+template<typename Numeric>
+inline quat operator/(const Numeric& val, const quat& q)
+{
+	static_assert(std::is_integral<Numeric>::value || std::is_floating_point<Numeric>::value,
+		"Numeric type must be an integer or floating point value.");
+	return quat(val / q.x, val / q.y, val / q.z, val / q.a);
+}
 
 inline std::ostream& operator<<(std::ostream& out, const quat& q)
 {
--- src/unittest/math/quat_unittest.cpp
+++ src/unittest/math/quat_unittest.cpp
@@ -46,13 +46,53 @@ public:
 		(q /= 3) /= 2;
 		Assert::AreEqual(quat(5, 6, 7, 8), q);
 
-		// NOTE(ref2401): Hamilton product is tested in the binary_operators method
+		// NOTE(ref2401): Hamilton product compaund assignment operator 
+		// is tested in the binary_operator test method.
 	}
 
 
 	TEST_METHOD(binary_operator)
 	{
+		quat q(1, 2, 3, 4);
 
+		Assert::AreEqual(quat(1.11f, 2.22f, 3.33f, 4.44f), q + quat(0.11f, 0.22f, 0.33f, 0.44f));
+		Assert::AreEqual(quat(1.11f, 2.22f, 3.33f, 4.44f), quat(0.11f, 0.22f, 0.33f, 0.44f) + q);
+
+		Assert::AreEqual(quat(0, 1, 2, 3), q - quat(1, 1, 1, 1));
+		Assert::AreEqual(quat(0, -1, -2, -3), quat(1, 1, 1, 1) - q);
+
+		Assert::AreEqual(quat(10, 20, 30, 40), q * 10);
+		Assert::AreEqual(quat(10, 20, 30, 40), 10 * q);
+
+		Assert::AreEqual(quat(0.5f, 1, 1.5f, 2), q / 2);
+		Assert::AreEqual(quat(2, 1, 2.f / 3, 0.5f), 2 / q);
+
+		// Hamilton product
+		quat neg_identity(0, 0, 0, -1);
+		Assert::AreEqual(neg_identity, quat::i * quat::i);
+		Assert::AreEqual(neg_identity, quat::j * quat::j);
+		Assert::AreEqual(neg_identity, quat::k * quat::k);
+		Assert::AreEqual(neg_identity, (quat::i * quat::j) * quat::k);
+
+		Assert::AreEqual(quat::k, quat::i * quat::j, L"ij = k");
+		Assert::AreEqual(quat::i, quat::j * quat::k, L"jk = i");
+		Assert::AreEqual(quat::j, quat::k * quat::i, L"ki = j");
+		Assert::AreEqual(-quat::k, quat::j * quat::i, L"ji = -k");
+		Assert::AreEqual(-quat::i, quat::k * quat::j, L"kj = -i");
+		Assert::AreEqual(-quat::j, quat::i * quat::k, L"ik = -j");
+
+		quat p(2, 3, 4, 1);
+		quat r(10, 11, 12, 9);
+
+		Assert::AreEqual(p * (q * r), (p * q) * r);
+		Assert::AreEqual(p*(5*q + 7*r), 5*(p*q) + 7*(p*r), L"linearity: p(5q + 7r) = 5pq + 7pr");
+		Assert::AreEqual((5*p + 7*q)*r, 5*(p*r) + 7*(q*r), L"linearity: (5p + 7q)r = 5pr + 7qr");
+
+		Assert::AreEqual(q*5, q*quat(0, 0, 0, 5));
+
+		// Hamilton product compaund assignment operator
+		quat p1 = p;
+		Assert::AreEqual(p * q * r, (p1 *= q) *= r);
 	}
 
 	TEST_METHOD(equal_operator)
@@ -66,6 +106,14 @@ public:
 
 		Assert::AreEqual(v, v);
 		Assert::AreEqual(v, quat(1, 2, 3, 4));
+	}
+
+	TEST_METHOD(unary_operators)
+	{
+		quat q(1, 2, 3, 4);
+
+		Assert::AreEqual(quat(-1, -2, -3, -4), -q);
+		Assert::AreEqual(quat(1, 2, 3, 4), -(-q));
 	}
 };
 
