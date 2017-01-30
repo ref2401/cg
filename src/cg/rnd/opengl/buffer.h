4f9bc1d315a16a9461c23599d862c93cde587f8d
ref2401 <refactor24@gmail.com>
2017-01-30 16:01:39 +0300

    Buffer_partitioned

--- src/cg/rnd/opengl/buffer.h
+++ src/cg/rnd/opengl/buffer.h
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <array>
 #include <type_traits>
+#include <utility>
 #include <vector>
 #include "cg/base/container.h"
 #include "cg/rnd/opengl/opengl_def.h"
@@ -207,6 +208,131 @@ size_t Buffer_persistent_map::write(size_t offset, const T* ptr, size_t count) n
 	std::memcpy(_ptr + offset, ptr, sizeof(T) * count);
 	return ret_offset;
 }
+
+// Partitioned_buffer is a buffer decorator that implements double/triple/whatever_factor buffering technique.
+// Buffering technique is supposed to solve CPU-GPU synchronization problem.
+// Partitioned_buffer creates an internal object of type Buffer_type 
+// which is partitioned into subbufer_count equal chunks. 
+// At every time Partitioned_buffer writes only into the specific(active/current) chunk of the internal buffer.
+template<typename Buffer_type>
+class Buffer_partitioned final {
+
+	static_assert(std::is_same<Buffer_type, Buffer_persistent_map>::value,
+		"Only Buffer_persistent_map as Buffer_type is supported at the moment.");
+
+public:
+
+	// Creates object of Buffer_type which size is byte_count * subbuffer_count bytes.
+	Buffer_partitioned(size_t partition_count, size_t partition_byte_count) noexcept
+		: _partition_count(partition_count), _partition_byte_count(partition_byte_count),
+		_buffer(partition_count * partition_byte_count)
+	{}
+
+	Buffer_partitioned(const Buffer_partitioned&) = delete;
+
+	Buffer_partitioned(Buffer_partitioned&& bp) noexcept;
+
+	~Buffer_partitioned() noexcept = default;
+
+
+	Buffer_partitioned& operator=(const Buffer_partitioned&) = delete;
+
+	Buffer_partitioned& operator=(Buffer_partitioned&& bp) noexcept;
+
+
+	// The index of partition which is currently used as target for writes.
+	// The number lies in range [0, partition_count).
+	size_t current_partition_index() const noexcept
+	{
+		return _partition_offset / _partition_byte_count;
+	}
+
+	GLuint id() const noexcept
+	{
+		return _buffer.id();
+	}
+
+	// Makes current the next buffer partition. If the end of the internal buffer has been reached
+	// then the first partition (index: 0) will be current.
+	void move_next_partition() noexcept
+	{
+		size_t next_index = (current_partition_index() + 1) % _partition_count;
+		_partition_offset = next_index * _partition_byte_count;
+	}
+
+	// How many bytes are occupies by each partition of the internal buffer.
+	size_t partition_byte_count() const noexcept
+	{
+		return _partition_byte_count;
+	}
+
+	// The number of partitions of the internal buffer.
+	size_t partition_count() const noexcept
+	{
+		return _partition_count;
+	}
+
+	// Writes a sequence of objects of type T into the current partiotion of the internal buffer.
+	// Params:
+	// -	rel_offset: Byte offset from the beginig of the current partition.
+	// -	ptr: Pointer to the first object of type T in the sequence.
+	// -	count: How many objects are in the sequence pointed by ptr.
+	//		If count is 0 the method returns immediately.
+	// Returns:	rel_offset + sizeof(T) * count.
+	//			Byte offset into the current partition that can be used for subsequent writes.	
+	template<typename T>
+	size_t write(size_t rel_offset, const T* ptr, size_t count = 1) noexcept;
+
+private:
+	size_t _partition_count = 0;
+	size_t _partition_byte_count = 0;
+	size_t _partition_offset = 0;
+	Buffer_type _buffer;
+};
+
+template<typename Buffer_type>
+Buffer_partitioned<Buffer_type>::Buffer_partitioned(Buffer_partitioned&& bp) noexcept
+	: _partition_count(bp._partition_count),
+	_partition_byte_count(bp._partition_byte_count),
+	_partition_offset(bp._partition_offset)
+	_buffer(std::move(bp._buffer))
+{
+	bp._partition_count = 0;
+	bp._partition_byte_count = 0;
+	bp._partition_offset = 0;
+}
+
+template<typename Buffer_type>
+Buffer_partitioned<Buffer_type>& Buffer_partitioned<Buffer_type>::operator=(Buffer_partitioned&& bp) noexcept
+{
+	if (this == &bp) return *this;
+
+	_partition_count = bp._partition_count;
+	_partition_byte_count = bp._partition_byte_count;
+	_partition_offset = bp._partition_offset;
+	_buffer = std::move(bp._buffer);
+
+	bp._partition_count = 0;
+	bp._partition_byte_count = 0;
+	bp._partition_offset = 0;
+
+	return *this;
+}
+
+template<typename Buffer_type>
+template<typename T>
+size_t Buffer_partitioned<Buffer_type>::write(size_t rel_offset, const T* ptr, size_t count) noexcept
+{
+	static_assert(std::is_trivially_copy_assignable<T>::value, "T must satisfy is_trivially_copy_assignable.");
+	if (count == 0) return rel_offset;
+
+	size_t ret_offset = rel_offset + sizeof(T) * count;
+	assert(ret_offset <= _partition_byte_count);
+
+	_buffer.write<T>(_partition_offset + rel_offset, ptr, count);
+	return ret_offset;
+}
+
 
 // Copies contents of the src buffer to the dest buffer.
 // Params:
